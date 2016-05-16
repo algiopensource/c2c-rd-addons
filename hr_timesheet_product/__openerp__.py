f2282d5378767d47028fd084d5266a32363c74c7
cesaralgios <cesar.algios@gmail.com>
2016-05-16 11:11:10 +0200

    Update __openerp__.py

--- hr_timesheet_product/__openerp__.py
+++ hr_timesheet_product/__openerp__.py
@@ -31,7 +31,7 @@ This module displays the hidden field product_id
 , 'depends'     : ['hr_timesheet' ]
 , 'data'  : ['hr_timesheet_product.xml']
 , 'demo_xml'    : []
-, 'installable': False
-, 'active'      : False
+, 'installable': True
+, 'active'      : True
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
